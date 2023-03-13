453c10d2020d29862df8ef19790c0a2d098e7af2
s24834 <s24834@pjwstk.edu.pl>
2023-03-13 22:22:32 +0100

    4. modified script (browser archive download)

--- new1.py
+++ new1.py
@@ -1,3 +1,13 @@
-import webbrowser #pakiet pythona
 url = input("podaj adres strony")
-webbrowser.open(url)
+
+import requests # moduł requests pozwala na wysyłanie żądań HTTP wykorzystując Pythona
+versions_dates = ["20231410", "20221410", "20211410"] # lista z datami archiwalnymi
+
+for date in versions_dates:
+    j = requests.get(f"https://archive.org/wayback/available?url={url}&timestamp={date}").json() # słownik
+    archive = j["archived_snapshots"]["closest"]["url"] # pozyskanie url potrzebnego do parametru metody .get()
+    browser = requests.get(archive).text # text pozyskuje zawartość request'owanego pliku
+    with open(f"{date}.html", "w") as b: # metoda open() otwiera plik, na którym będę pracować; open() przyjmuje 3 parametry [nazwa] [mode, czyli w=write, r=read, a=append] [encoding]
+        # with open() działa tak samo jak open(), z tą różnicą, że with open() sam zamyka plik (bez metody nazwa_pliku.close())
+        b.write(browser) # write wypisuje do podanego pliku podany w argumencie text (z opcją "w" całą zawartość pliku usuwa i wstawia dany, nowy tekst)
+
